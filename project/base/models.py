55dbbbf56170d0045c03a2e4fb29e89b45cc71e0
vebesz <barnabas.varszegi@gmail.com>
2022-10-19 21:26:47 +0200

    Music model

--- project/base/models.py
+++ project/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Music(models.Model):
+    title = models.TextField()
+    artist = models.TextField()
+    image = models.ImageField(blank=True, null=True)
+    audio = models.FileField()
+    duration = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
